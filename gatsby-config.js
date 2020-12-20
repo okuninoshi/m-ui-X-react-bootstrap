c148bcb22260dcf2ce13774c2e201bfb48b40add
okuninoshi <gennesis@outlook.fr>
2020-12-20 14:53:52 +0100

    add plugins configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,36 @@
 module.exports = {
     plugins: [
         {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `pages`,
+                path: `${__dirname}/src/pages/`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `data`,
+                path: `${__dirname}/src/data/`,
+                ignore: [`**/\.*`], // ignore files starting with a dot
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                // CommonMark mode (default: true)
+                commonmark: true,
+                // Footnotes mode (default: true)
+                footnotes: true,
+                // Pedantic mode (default: true)
+                pedantic: true,
+                // GitHub Flavored Markdown mode (default: true)
+                gfm: true,
+                // Plugins configs
+                plugins: [],
+            },
+        },
+        {
             resolve: `gatsby-plugin-material-ui`,
             options: {
                 stylesProvider: {
